1ec14b65dd4a87e4efdba79453df4f17acf29fc8
olawuni50 <Olawuni50@gmail.com>
2020-11-22 04:29:41 +0100

    initial commit

--- Questions and Answers/Questions and Solutions.py
+++ Questions and Answers/Questions and Solutions.py
@@ -242,3 +242,42 @@ elif alpha == 'y':
     print(f"{alpha} could be Vowel or Consonant")
 else:
     print(f"{alpha} is Consonant")
+
+
+
+                                            #14
+"""
+Create a program that asks the user to enter their name and their age. Print out a message addressed to them that
+tells them the year that they will turn 100 years old.
+"""
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+year = int(input("Enter present year: "))
+my_future_year = (2020 - age) + 100
+print(f"{name} will be 100 years in {my_future_year}")
+
+
+                                            #15
+"""
+Take a list, say for example this one:
+a=[1,1,2,3,5,8,13,21,34,55,89]
+and write a program that prints out all the elements of the list that are less than 5
+Extras:
+1. Instead of printing the elements one by one, make a new list that has all the elements less than 5
+from this list in it and print out this new list.
+2. Write this in one line of Python.
+3. Ask the user for a number and return a list that contains only elements from the original list
+'a' that are smaller than that number given by the user.
+
+"""
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [c for c in a if c < 5]                     #list comprehension
+print(b)
+while True:
+    number = int(input("Enter number: "))
+    if number in a:
+        break
+    print("Enter correct number")
+d = [f for f in a if f < number]                #list comprehension
+print(d)
